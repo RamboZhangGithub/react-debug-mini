33ef0c90e8e1c093a56e5c1dbbbe86b806059f16
张国浩 <zhangguohao@codemao.cn>
2021-11-29 17:24:29 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// import * as React from 'react';
+// import * as ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+
+import { ReactDOM } from "./whichReact";
+import "./index.css";
+import SetStatePage from "./pages/SetStatePage";
+
+ReactDOM.render(<SetStatePage />, document.getElementById("root"));
+
+// ReactDOM.createRoot(document.getElementById("root")).render(<SetStatePage />);
+
+// console.log("React", React.version); //sy-log
